9f5ef5d62f69ba6480199a4637fca818db648a31
Charles Xing <zhinoooob@gmail.com>
2026-03-25 13:59:26 -0700

    Updated decoder.h again -Charles

--- decoder.h
+++ decoder.h
@@ -5,3 +5,8 @@ void initialize_timer22(void);
 int decode(int signal_length);
 
 extern volatile int command_signal;
+extern volatile int pulse_width;
+extern volatile int signal_flag;
+extern volatile int signal_length = 0;
+extern volatile int signal_start = 0;
+extern volatile int capture = 0;
