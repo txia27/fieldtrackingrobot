b5e5d6cc5227eb92549a4bad31b8d62f0e3c00c8
erichob1234 <ericwang4222@gmail.com>
2026-03-24 18:10:24 -0700

    adc

---
+++ ADC.c
@@ -0,0 +1,51 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "stm32l051xx.h"
+
+void ADC_Init(void)
+{
+    //Enable clocks
+    RCC->IOPENR |= RCC_IOPENR_GPIOAEN;
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+
+    //Set PA4, PA5, PA6 to analog mode
+    GPIOA->MODER |= (3 << 8) | (3 << 10) | (3 << 12);
+    GPIOA->PUPDR &= ~((3 << 8) | (3 << 10) | (3 << 12));
+
+    //Disable ADC if already enabled
+    if (ADC1->CR & ADC_CR_ADEN)
+    {
+        ADC1->CR |= ADC_CR_ADDIS;
+        while (ADC1->CR & ADC_CR_ADEN);
+    }
+
+    //Enable ADC voltage regulator
+    ADC1->CR |= ADC_CR_ADVREGEN;
+    for (volatile int i = 0; i < 1000; i++); // delay
+
+    //Sampling time (VERY IMPORTANT for your circuit)
+    ADC1->SMPR |= (7 << ADC_SMPR_SMP_Pos);  // max sample time
+
+    //Enable ADC
+    ADC1->CR |= ADC_CR_ADEN;
+    while (!(ADC1->ISR & ADC_ISR_ADRDY));
+}
+
+uint16_t ADC_Read_Channel(uint8_t ch)
+{
+    ADC1->CHSELR = (1 << ch);
+
+    ADC1->CR |= ADC_CR_ADSTART;
+
+    uint32_t timeout = 100000;
+
+    while (!(ADC1->ISR & ADC_ISR_EOC))
+    {
+        if (--timeout == 0)
+        {
+            return 61; // fail safe
+        }
+    }
+
+    return ADC1->DR;
+}
---
+++ ADC.h
@@ -0,0 +1,10 @@
+#ifndef ADC_H
+#define ADC_H //just to tell preprocessor this file is already processed
+
+#include <stdint.h>
+#include <stdbool.h>
+
+void ADC_Init(void);
+uint16_t ADC_Read(uint8_t ch);
+
+#endif
