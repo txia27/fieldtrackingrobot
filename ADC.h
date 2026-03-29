e7c14b2e3a2ee925c3755b68115566167e6e2de6
Alek <faleksa2006@gmail.com>
2026-03-29 15:41:14 -0700

    Alek ADC Again

--- ADC.h
+++ ADC.h
@@ -5,6 +5,7 @@
 #include <stdbool.h>
 
 void ADC_Init(void);
-uint16_t ADC_Read_Channel(uint8_t ch);
+//uint16_t ADC_Read_Channel(uint8_t ch);
+void ADC_Read_All(uint16_t *ch4, uint16_t *ch5, uint16_t *ch6);
 
 #endif 	
